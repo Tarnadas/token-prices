fc32b8889dff167506c5f4a019375f1674c78fb8
Mario Reder <mario.reder@pm.me>
2023-02-13 13:38:45 +0100

    save all prices

--- src/get-prices.js
+++ src/get-prices.js
@@ -1,18 +1,11 @@
 import fetch from "node-fetch"
-import { pick } from "ramda"
-
-const TOKENS = [
-  "meta-token.near",
-  "token.v2.ref-finance.near",
-  "token.burrow.near",
-]
 
 const main = async () => {
   const prices = await fetch(
     "https://indexer.ref.finance/list-token-price",
   ).then((res) => res.json())
 
-  console.info(JSON.stringify(pick(TOKENS, prices)))
+  console.info(prices)
 }
 
 main()
